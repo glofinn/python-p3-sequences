645cf017f341df2a4b1f33ebb61efe26c679254c
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-03-13 15:49:40 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    # if length > 0:
+    #     fibonacci_sequence.append(0)
+    #     if length > 1:
+    #         fibonacci_sequence.append(1)
+    #         if length > 2:
+    #             for i in range(2, length):
+    #                 fibonacci_sequence.append(
+    #                     fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+    if length == 0:
+        fibonacci_sequence = []
+    elif length == 1:
+        fibonacci_sequence = [0]
+    elif length == 2:
+        fibonacci_sequence = [0,1]
+    elif length > 2:
+        fibonacci_sequence = [0,1]
+        for i in range(2, length):
+            fibonacci_sequence.append(
+            fibonacci_sequence[i -1] + fibonacci_sequence[i - 2])
+    
+
+    print(fibonacci_sequence)
